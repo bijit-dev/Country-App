4c51acc6dff5655ef850c7d875ca69c2f1767604
BIJIT DEB <bijitdeb0703@gmail.com>
2025-03-31 22:34:04 +0600

    flag add button add

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries, handleVisitedFlag }) => {
     const [visited, setVisited] = useState(false);
+
     function handleVisited() {
         setVisited(!visited);
+        visited? "":handleVisitedCountries(country);
     }
+
 
     return (
         <div className={`border-2 border-blue-500 p-5 m-3 flex flex-col items-center justify-cente gap-3 ${visited && "bg-emerald-400"} `}>
             <h1 className=" font-bold text-2xl">Name: {country.name.common}</h1>
             <div className="h-28"><img className="w-full h-full " src={country.flags.png} alt="" /></div>
             <h2 className="font-semibold text-2xl">population: {country.population}</h2>
+
+            {/* <h2>languages: {lan}</h2> */}
+            <button onClick={handleVisited} className={`border border-red-500 rounded-xl px-5 py-1  text-white text-xl font-semibold  ${visited ? "bg-amber-600" : "bg-emerald-500"} `}>{visited ? "Visited" : "Not visited"}</button>
             
-            <button onClick={handleVisited} className={`border border-red-500 rounded-xl px-5 py-1  text-white text-xl font-semibold  ${visited ?"bg-amber-600": "bg-emerald-500"} `}>{visited? "Visited": "Not visited"}</button>
+            <button onClick={()=>handleVisitedFlag(country.flags.png)}>add visited Flag</button>
             
         </div>
     );
